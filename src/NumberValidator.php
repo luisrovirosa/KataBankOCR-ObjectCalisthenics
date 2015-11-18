977ebe238c21d1855fd10b581c57a10f26debfaf
Luis Rovirosa <luisrovirosa@gmail.com>
2015-11-18 19:11:11 +0100

    NumberValidator refactor

--- src/NumberValidator.php
+++ src/NumberValidator.php
@@ -4,7 +4,31 @@ namespace KataBank;
 
 class NumberValidator
 {
+    /**
+     * @param \KataBank\Number $number
+     * @throws NumberNotValidException
+     */
     public function validate(Number $number)
+    {
+        if (!$this->hasValidChecksum($number)) {
+            throw new NumberNotValidException();
+        }
+    }
+
+    /**
+     * @param \KataBank\Number $number
+     * @return bool
+     */
+    private function hasValidChecksum(Number $number)
+    {
+        return ($this->checksum($number) % 11) === 0;
+    }
+
+    /**
+     * @param \KataBank\Number $number
+     * @return int
+     */
+    private function checksum(Number $number)
     {
         $checksum = 0;
         $numberOfDigit = count($number->digits());
@@ -12,8 +36,7 @@ class NumberValidator
             $checksum += $numberOfDigit * $digit->value();
             $numberOfDigit--;
         }
-        if (($checksum % 11) !== 0) {
-            throw new NumberNotValidException();
-        }
+
+        return $checksum;
     }
 }
