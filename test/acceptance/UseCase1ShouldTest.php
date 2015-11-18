d2fec5939399796bc518bc24d921b25a23c8c42b
Luis Rovirosa <luisrovirosa@gmail.com>
2015-11-18 19:40:44 +0100

    First acceptance test all ones

--- test/acceptance/UseCase1ShouldTest.php
+++ test/acceptance/UseCase1ShouldTest.php
@@ -2,7 +2,12 @@
 
 namespace KataBank\Test\Acceptance;
 
+use KataBank\DigitBuilder;
+use KataBank\NumberBuilder;
+use KataBank\NumberValidator;
+use KataBank\Ocr;
 use KataBank\Test\BaseTest;
+use KataBank\TextDigits;
 
 class UseCase1ShouldTest extends BaseTest
 {
@@ -13,7 +18,7 @@ class UseCase1ShouldTest extends BaseTest
      */
     public function find_the_number($expectedNumber, $text)
     {
-        $ocr = $this->realOcr();
+        $ocr = $this->ocr();
 
         $number = $ocr->read($text);
 
@@ -27,9 +32,27 @@ class UseCase1ShouldTest extends BaseTest
             "| || || || || || || || || |\n" .
             "|_||_||_||_||_||_||_||_||_|\n" .
             "                           \n";
+        $allOnes =
+            "                           \n" .
+            "  |  |  |  |  |  |  |  |  |\n" .
+            "  |  |  |  |  |  |  |  |  |\n" .
+            "                           \n";
 
         return [
-            ["000000000", $allZeros]
+            ["000000000", $allZeros],
+            ["111111111", $allOnes],
         ];
+    }
+
+    private function ocr()
+    {
+        $textDigits = new TextDigits();
+        $digitBuilder = new DigitBuilder($textDigits);
+        $numberBuilder = new NumberBuilder($digitBuilder);
+        /** @var NumberValidator $validator */
+        $validator = $this->prophesize('KataBank\NumberValidator')->reveal();
+        $ocr = new Ocr($numberBuilder, $validator);
+
+        return $ocr;
     }
 }
