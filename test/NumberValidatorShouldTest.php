f04f7e6aa85fe652d9a607fecf57cd654df6c202
Luis Rovirosa <luisrovirosa@gmail.com>
2015-11-18 19:02:37 +0100

    NumberValidator should do_noting_when_the_number_is_correct

---
+++ test/NumberValidatorShouldTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace KataBank\Test;
+
+use KataBank\Digit;
+use KataBank\Number;
+use KataBank\NumberValidator;
+
+class NumberValidatorShouldTest extends BaseTest
+{
+    /** @test */
+    public function do_noting_when_the_number_is_correct()
+    {
+        $validator = new NumberValidator();
+        $digits = [
+            new Digit(3),
+            new Digit(4),
+            new Digit(5),
+            new Digit(8),
+            new Digit(8),
+            new Digit(2),
+            new Digit(8),
+            new Digit(6),
+            new Digit(5),
+        ];
+        $validNumber = new Number($digits);
+        $validator->validate($validNumber);
+    }
+}
