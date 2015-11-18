b371c2b007517c25c41d8590e23af9c87263a574
Luis Rovirosa <luisrovirosa@gmail.com>
2015-11-18 19:07:19 +0100

    NumberValidator should throw_an_exception_when_the_number_is_not_correct

--- src/NumberValidator.php
+++ src/NumberValidator.php
@@ -6,5 +6,14 @@ class NumberValidator
 {
     public function validate(Number $number)
     {
+        $checksum = 0;
+        $numberOfDigit = count($number->digits());
+        foreach ($number->digits() as $digit) {
+            $checksum += $numberOfDigit * $digit->value();
+            $numberOfDigit--;
+        }
+        if (($checksum % 11) !== 0) {
+            throw new NumberNotValidException();
+        }
     }
 }
--- test/NumberValidatorShouldTest.php
+++ test/NumberValidatorShouldTest.php
@@ -26,4 +26,24 @@ class NumberValidatorShouldTest extends BaseTest
         $validNumber = new Number($digits);
         $validator->validate($validNumber);
     }
+
+    /** @test */
+    public function throw_an_exception_when_the_number_is_not_correct()
+    {
+        $this->setExpectedException('KataBank\NumberNotValidException');
+        $validator = new NumberValidator();
+        $digits = [
+            new Digit(3),
+            new Digit(4),
+            new Digit(5),
+            new Digit(8),
+            new Digit(8),
+            new Digit(2),
+            new Digit(8),
+            new Digit(6),
+            new Digit(6),
+        ];
+        $validNumber = new Number($digits);
+        $validator->validate($validNumber);
+    }
 }
